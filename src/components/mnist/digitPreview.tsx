3e055866a69f8efb362541b61107d5bf79891e3c
nosson <nossonhuebner@gmail.com>
2023-11-19 12:17:02 -0500

    Truncate loss

--- src/components/mnist/digitPreview.tsx
+++ src/components/mnist/digitPreview.tsx
@@ -34,7 +34,7 @@ function DigitPreview({item}: {item: ImageItem}) {
             <Stack direction="row" style={{width: 'fit-content'}}>
                 <canvas ref={mnistRef} height="28" width="28" className="digitCanvas"/>
                 <div style={{width: '80px', fontSize: '12px'}}>
-                    {loss && (<div>{loss}</div>)}
+                    {loss && (<div>{loss.toFixed(5)}</div>)}
                     {preds && topPredIdx?.map(pIdx => {
                         const color = pIdx == label ? 'green' : 'red';
                         return (
